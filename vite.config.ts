33c5ead345d0d67685cf80e4f7fd23819d80342f
Don't have a name <gollapagus@gmail.com>
2026-01-28 06:19:26 +0200

    Update base path to /PWAWCMC/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pwawcmc/",
+  base: "/PWAWCMC/",
   build: {
     sourcemap: true,
     assetsDir: "code",
